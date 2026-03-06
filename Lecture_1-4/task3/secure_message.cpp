3e38a63d7d594b2693aae488653d8597c27313a6
stormdyn <bretokmine@gmail.com>
2026-03-06 13:42:12 +0300

    task3-v0.1

---
+++ Lecture_1-4/task3/secure_message.cpp
@@ -0,0 +1,152 @@
+#include <utility>
+#include "secure_message.hpp"
+
+namespace context7
+{
+    void SecureMessage::copy_buffer(const char* src, size_t len) 
+    {
+        for (size_t i = 0; i < len; ++i) {
+            data_[i] = src[i];
+        }
+        data_[len] = '\0';
+    }
+
+    size_t SecureMessage::compute_length(const char* str)  
+    {
+        if (!str) {
+            return 0;
+        }
+        size_t len = 0;
+        while (str[len] != '\0') {
+            ++len;
+        }
+        return len;
+    }
+
+    SecureMessage::SecureMessage() : data_(nullptr), length_(0) {}
+
+    SecureMessage::SecureMessage(const char* str)
+    {
+        if (!str) {
+            data_ = nullptr;
+            length_ = 0;
+            return;
+        }
+        
+        length_ = compute_length(str);
+        data_ = new char[length_ + 1]; // Если нет памяти, вылетит std::bad_alloc
+        copy_buffer(str, length_);
+    }
+
+    SecureMessage::SecureMessage(const SecureMessage& other)
+    {
+        length_ = other.length_;
+        if (length_ == 0 || !other.data_) {
+            data_ = nullptr;
+            return;
+        }
+        
+        data_ = new char[length_ + 1];
+        copy_buffer(other.data_, length_);
+    }
+
+    SecureMessage& SecureMessage::operator=(const SecureMessage& other)
+    {
+        if (this == &other) 
+        {
+            return *this;
+        }
+        
+        if (other.length_ == 0 || !other.data_) 
+        {
+            delete[] data_;
+            data_ = nullptr;
+            length_ = 0;
+            return *this;
+        }
+        
+        // Строгая гарантия исключений: если new бросит исключение,
+        // старые data_ и length_ не пострадают
+        char* tmp = new char[other.length_ + 1];
+        for (size_t i = 0; i <= other.length_; ++i) 
+        {
+            tmp[i] = other.data_[i];
+        }
+        
+        delete[] data_;
+        data_ = tmp;
+        length_ = other.length_;
+        
+        return *this;
+    }
+
+    SecureMessage::SecureMessage(SecureMessage&& other) noexcept
+        : data_(other.data_), length_(other.length_) 
+    {
+        other.data_ = nullptr;
+        other.length_ = 0;
+    }
+
+    SecureMessage& SecureMessage::operator=(SecureMessage&& other) noexcept {
+        if (this == &other) 
+        {
+            return *this;
+        }
+        delete[] data_;
+        data_ = std::exchange(other.data_, nullptr);
+        length_ = std::exchange(other.length_, 0);
+        return *this;
+    }
+
+    SecureMessage::~SecureMessage() {
+        delete[] data_;
+    }
+
+    const char* SecureMessage::get_data() const {
+        return data_;
+    }
+
+    size_t SecureMessage::get_length() const {
+        return length_;
+    }
+
+
+    bool SecureMessage::operator==(const SecureMessage& other) const
+    {
+        if (length_ != other.get_length())
+        {
+            return false;
+        }
+
+        for (int i = 0; i < length_; i++)
+        {
+            if (data_[i] != other.get_data()[i])
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    SecureMessage SecureMessage::operator+(const SecureMessage& other) const
+    {
+        SecureMessage result(data_);
+
+        result.length_ = length_ + other.get_length();
+        delete [] result.data_;
+        result.data_ = new char[result.length_ + 1];
+
+        for (int i = 0; i < length_; i++)
+        {
+            result.data_[i] = data_[i];
+        }
+
+        for (int i = 0; i < other.get_length(); i++)
+        {
+            result.data_[i + length_] = other.data_[i];
+        }
+
+        return result;
+    }
+}
---
+++ Lecture_1-4/task3/secure_message.hpp
@@ -0,0 +1,32 @@
+#include <cstddef>
+#include <ostream>
+
+namespace context7 {
+
+    class SecureMessage {
+    private:
+        char*  data_;
+        size_t length_;
+        size_t compute_length(const char* str);
+        
+        void copy_buffer(const char* src, size_t len);
+    public:
+        SecureMessage();
+        SecureMessage(const char* str);
+        SecureMessage(const SecureMessage& other);
+        SecureMessage& operator=(const SecureMessage& other);
+        SecureMessage(SecureMessage&& other) noexcept;
+        SecureMessage& operator=(SecureMessage&& other) noexcept;
+        ~SecureMessage();
+
+        bool operator==(const SecureMessage& other) const;
+        SecureMessage operator+(const SecureMessage& other) const;
+        SecureMessage& operator+=(const SecureMessage& other);
+        char& operator[](size_t index);
+        const char& operator [](size_t index) const;
+        friend std::ostream& operator<<(std::ostream& os, const SecureMessage& msg);
+
+        const char* get_data() const;
+        size_t      get_length() const;
+    };
+}
